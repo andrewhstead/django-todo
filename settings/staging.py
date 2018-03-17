76ab67c9ea641370ebdc6b2f119bf8544c12a781
Andrew Stead <andrew@andrewstead.co.uk>
2018-03-17 16:59:57 +0000

    Separating code

---
+++ settings/staging.py
@@ -0,0 +1,33 @@
+from base import *
+
+DEBUG = False
+
+# Database
+# https://docs.djangoproject.com/en/1.11/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+SITE_URL = 'https://django-todo-code-institute.herokuapp.com'
+ALLOWED_HOSTS.append('django-todo-code-institute.herokuapp.com')
+
+# Log DEBUG information to the console
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
+        },
+    },
+}
