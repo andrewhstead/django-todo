3916f65e7f5aa0d33a838b8c232c032a97bec7ef
Andrew Stead <andrew@andrewstead.co.uk>
2018-03-17 23:13:36 +0000

    CORS settings

--- settings/base.py
+++ settings/base.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'todo.apps.TodoConfig',
     'rest_framework',
     'accounts',
+    'corsheaders',
 ]
 
 SECURE_SSL_REDIRECT = True
@@ -67,6 +68,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'django_todo.urls'
@@ -129,3 +131,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = True
+CORS_ORIGIN_WHITELIST = (
+    'http://localhost:8080',
+    'http://127.0.0.1:8080',
+)
